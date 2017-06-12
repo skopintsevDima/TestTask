c8ff9929dbd1c7655f38bcf7a31ef9fbd29fc568
Skopintsev Dima <skopintsev2015@gmail.com>
2017-06-12 23:08:53 +0300

    Sign in added

--- app/src/main/java/com/skopincev/testtask/MainActivity.java
+++ app/src/main/java/com/skopincev/testtask/MainActivity.java
@@ -1,27 +1,46 @@
 package com.skopincev.testtask;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements
         OnConnectionFailedListener,
         OnClickListener {
 
+    private static final int RC_SIGN_IN = 1;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     private GoogleApiClient mGoogleApiClient;
+
+    @BindView(R.id.btn_sign_in)
+    SignInButton btn_sign_in;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initGoogleApiClient();
     }
 
     private void initGoogleApiClient(){
@@ -33,15 +52,61 @@ public class MainActivity extends AppCompatActivity implements
                 .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
                 .build();
+
+        if (btn_sign_in != null) {
+            btn_sign_in.setSize(SignInButton.SIZE_STANDARD);
+            btn_sign_in.setOnClickListener(this);
+        }
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        Log.d(TAG, connectionResult.getErrorMessage());
     }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_sign_in:{
+                signIn();
+                break;
+            }
+        }
+    }
 
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            GoogleSignInAccount account = result.getSignInAccount();
+            if (account != null) {
+                String userEmail = account.getEmail();
+                Log.d(TAG, "userEmail = " + userEmail);
+                openContactsList(userEmail);
+            }
+            else {
+                Log.d(TAG, "Account is NULL!");
+            }
+        } else {
+            Toast.makeText(this, R.string.sign_in_failed_message, Toast.LENGTH_SHORT)
+            .show();
+        }
+    }
+
+    private void openContactsList(String userEmail){
+        //Intent intent = new Intent(this, );
     }
 }
